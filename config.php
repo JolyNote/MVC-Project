542c637bfef2b15f3e53330981d943a107c4be1a
is42-rusanovrv <romdek174@gmial.com>
2022-03-17 13:57:05 +0500

    Version 1.0.6

--- config.php
+++ config.php
@@ -1,6 +1,6 @@
 <?php
 const DB_HOSTNAME = 'mysqlis.radiotech.su';
-const DB_USERNAME = 'is342_20';
-const DB_PASSWORD = 'fNIJpUA30f';
-const DB_DATABASE = 'is342_20_MVC-Project';
+const DB_USERNAME = 'is342_20_test';
+const DB_PASSWORD = '4eGOAvrwAT';
+const DB_DATABASE = 'is342_20_test';
 const DB_PORT = '3306'; # Default 3306
